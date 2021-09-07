9286c36aa623f9dfcb238a0624199e1ba17cf29d
keithlim <keithlck96@gmail.com>
2021-09-07 15:39:19 +0800

    Implemented Baseline UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hrtech';
+
+  // for the setting of the side menu / mat-sidenav
+  public opened = true;
+
+  ngOnInit() {
+    window.innerWidth < 768 ? this.opened = false : this.opened = true;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    event.target.innerWidth < 768 ? this.opened = false : this.opened = true;
+  }
+
+  public isBiggerScreen() {
+    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    return width > 768;
+  }
+
 }
--- src/app/ng-material.module.ts
+++ src/app/ng-material.module.ts
@@ -1,14 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatDividerModule
   ],
   exports: [
-
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatDividerModule
   ]
 })
 export class NgMaterialModule { }
